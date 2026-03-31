e4b585c2e0052422e8165e19006cda7b670688af
Shahan-07 <iamshahanalam22@gmail.com>
2026-03-31 15:21:59 +0500

    Api Error handling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv"
 import conncetDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./.env"
 })
 
-conncetDB();
+conncetDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("mongo db conncetion failed", error);
+    
+})
